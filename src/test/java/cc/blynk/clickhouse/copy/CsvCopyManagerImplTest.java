5eb7a6fdc8862dd9db95249eaeae5f6c8dca0741
Dmitriy Sych <dmitriy.sych@intellectsoft.net>
2019-09-06 23:41:23 +0300

    fix checkstyles

--- src/test/java/cc/blynk/clickhouse/copy/CsvCopyManagerImplTest.java
+++ src/test/java/cc/blynk/clickhouse/copy/CsvCopyManagerImplTest.java
@@ -20,7 +20,6 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 public class CsvCopyManagerImplTest {
     private ClickHouseConnection connection;
