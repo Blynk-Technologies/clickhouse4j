c9fddd219abb83ffeacdea7b75114b7f84479b0e
Dmitriy Sych <dmitriy.sych@intellectsoft.net>
2019-09-08 22:12:37 +0300

    CopyManager added methods with additional DB params

--- src/main/java/cc/blynk/clickhouse/copy/CopyManager.java
+++ src/main/java/cc/blynk/clickhouse/copy/CopyManager.java
@@ -1,10 +1,13 @@
 package cc.blynk.clickhouse.copy;
 
+import cc.blynk.clickhouse.settings.ClickHouseQueryParam;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 import java.sql.SQLException;
+import java.util.Map;
 
 public interface CopyManager {
 
@@ -34,6 +37,28 @@ public interface CopyManager {
      *     InputStream sourceInputStream = ...
      *     String sql = "INSERT INTO default.my_table FORMAT CSV";
      *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyToDb(sql, sourceInputStream);
+     * </pre>
+     *
+     * @param sql                - SQL INSERT Query
+     * @param from               - InputStream with data
+     * @param additionalDBParams - specific DB params for request
+     * @throws SQLException - insertion error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyToDb(String sql,
+                  InputStream from,
+                  Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException;
+
+    /**
+     * Inserts the data from the stream to the DB.
+     * The type and structure of the data from the stream specified in the SQL Query.
+     * For example, in case you want to send the data in the CSV format to the DB you need to:
+     * <pre>
+     *     InputStream sourceInputStream = ...
+     *     String sql = "INSERT INTO default.my_table FORMAT CSV";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
      *     copyManager.copyToDb(sql, sourceInputStream, 1024);
      * </pre>
      *
@@ -44,6 +69,30 @@ public interface CopyManager {
      * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
      */
     void copyToDb(String sql, InputStream from, int bufferSize) throws SQLException;
+
+    /**
+     * Inserts the data from the stream to the DB.
+     * The type and structure of the data from the stream specified in the SQL Query.
+     * For example, in case you want to send the data in the CSV format to the DB you need to:
+     * <pre>
+     *     InputStream sourceInputStream = ...
+     *     String sql = "INSERT INTO default.my_table FORMAT CSV";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyToDb(sql, sourceInputStream, 1024);
+     * </pre>
+     *
+     * @param sql                - SQL INSERT Query
+     * @param from               - InputStream with data
+     * @param additionalDBParams - specific DB params for request
+     * @param bufferSize         - buffer size for the data transfer
+     * @throws SQLException - insertion error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyToDb(String sql,
+                  InputStream from,
+                  Map<ClickHouseQueryParam, String> additionalDBParams,
+                  int bufferSize)
+            throws SQLException;
 
     /**
      * Inserts the data from the stream to the DB.
@@ -78,6 +127,30 @@ public interface CopyManager {
      *     //  The data from the Reader will be forwarded to the DB in CSV format
      * </pre>
      *
+     * @param sql                - SQL INSERT Query
+     * @param from               - Reader with data
+     * @param additionalDBParams - specific DB params for request
+     * @throws SQLException - insertion error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyToDb(String sql,
+                  Reader from,
+                  Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException;
+
+    /**
+     * Inserts the data from the stream to the DB.
+     * The type and structure of the data from the stream specified in the SQL Query.
+     * For example, in case you want to send the data in the CSV format to the DB you need to:
+     * <pre>
+     *     Reader sourceReader = ...
+     *     String sql = "INSERT INTO default.my_table FORMAT CSV";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyToDb(sql, sourceReader);
+     *
+     *     //  The data from the Reader will be forwarded to the DB in CSV format
+     * </pre>
+     *
      * @param sql  - SQL INSERT Query
      * @param from - Reader with data
      * @param bufferSize - buffer size for the data transfer
@@ -85,6 +158,32 @@ public interface CopyManager {
      * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
      */
     void copyToDb(String sql, Reader from, int bufferSize) throws SQLException;
+
+    /**
+     * Inserts the data from the stream to the DB.
+     * The type and structure of the data from the stream specified in the SQL Query.
+     * For example, in case you want to send the data in the CSV format to the DB you need to:
+     * <pre>
+     *     Reader sourceReader = ...
+     *     String sql = "INSERT INTO default.my_table FORMAT CSV";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyToDb(sql, sourceReader);
+     *
+     *     //  The data from the Reader will be forwarded to the DB in CSV format
+     * </pre>
+     *
+     * @param sql                - SQL INSERT Query
+     * @param from               - Reader with data
+     * @param additionalDBParams - specific DB params for request
+     * @param bufferSize         - buffer size for the data transfer
+     * @throws SQLException - insertion error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyToDb(String sql,
+                  Reader from,
+                  Map<ClickHouseQueryParam, String> additionalDBParams,
+                  int bufferSize)
+            throws SQLException;
 
     /**
      * Loads the data from the DB using the SQL Query and writes it to the OutputStream
@@ -119,10 +218,59 @@ public interface CopyManager {
      *     copyManager.copyFromDb(sql, fos);
      * </pre>
      *
+     * @param sql                - SQL SELECT Query
+     * @param to                 - OutputStream to write data
+     * @param additionalDBParams - specific DB params for request
+     * @throws SQLException - loading error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyFromDb(String sql,
+                    OutputStream to,
+                    Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException;
+
+
+    /**
+     * Loads the data from the DB using the SQL Query and writes it to the OutputStream
+     * in the format defined in the SQL query.
+     * For example, in case you want to select the data from the DB and directly write
+     * it to the file in the csv format with the headers line and without creating intermediate
+     * objects in the application space:
+     * <pre>
+     *     FileOutputStream fos = ...
+     *     String sql = "SELECT * from default.my_table FORMAT CSVWithNames";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyFromDb(sql, fos);
+     * </pre>
+     *
      * @param sql - SQL SELECT Query
      * @param to - Writer to write the data to
      * @throws SQLException - loading error
      * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
      */
     void copyFromDb(String sql, Writer to) throws SQLException;
+
+    /**
+     * Loads the data from the DB using the SQL Query and writes it to the OutputStream
+     * in the format defined in the SQL query.
+     * For example, in case you want to select the data from the DB and directly write
+     * it to the file in the csv format with the headers line and without creating intermediate
+     * objects in the application space:
+     * <pre>
+     *     FileOutputStream fos = ...
+     *     String sql = "SELECT * from default.my_table FORMAT CSVWithNames";
+     *     CopyManager copyManager = CopyManagerFactory.create(connection);
+     *     copyManager.copyFromDb(sql, fos);
+     * </pre>
+     *
+     * @param sql                - SQL SELECT Query
+     * @param to                 - Writer to write the data to
+     * @param additionalDBParams - specific DB params for request
+     * @throws SQLException - loading error
+     * @see cc.blynk.clickhouse.domain.ClickHouseFormat for possible formats of the data
+     */
+    void copyFromDb(String sql,
+                    Writer to,
+                    Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException;
 }
--- src/main/java/cc/blynk/clickhouse/copy/CopyManagerImpl.java
+++ src/main/java/cc/blynk/clickhouse/copy/CopyManagerImpl.java
@@ -2,6 +2,7 @@ package cc.blynk.clickhouse.copy;
 
 import cc.blynk.clickhouse.ClickHouseConnection;
 import cc.blynk.clickhouse.ClickHouseDataSource;
+import cc.blynk.clickhouse.settings.ClickHouseQueryParam;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
@@ -9,6 +10,7 @@ import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 import java.sql.SQLException;
+import java.util.Map;
 
 final class CopyManagerImpl implements CopyManager, AutoCloseable {
 
@@ -31,6 +33,18 @@ final class CopyManagerImpl implements CopyManager, AutoCloseable {
      * {@inheritDoc}
      */
     @Override
+    public void copyToDb(String sql,
+                         InputStream from,
+                         Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException {
+        validate(sql, from);
+        connection.createStatement().sendStreamSQL(from, sql, additionalDBParams);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void copyToDb(String sql, InputStream from, int bufferSize) throws SQLException {
         validate(sql, from);
         BufferedInputStream bufferedStream = new BufferedInputStream(from, Math.max(32, bufferSize));
@@ -41,10 +55,37 @@ final class CopyManagerImpl implements CopyManager, AutoCloseable {
      * {@inheritDoc}
      */
     @Override
+    public void copyToDb(String sql,
+                         InputStream from,
+                         Map<ClickHouseQueryParam, String> additionalDBParams,
+                         int bufferSize)
+            throws SQLException {
+        validate(sql, from);
+        BufferedInputStream bufferedStream = new BufferedInputStream(from, Math.max(32, bufferSize));
+        connection.createStatement().sendStreamSQL(bufferedStream, sql, additionalDBParams);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void copyToDb(String sql, Reader from) throws SQLException {
         validate(sql, from);
         ReaderInputStream inputStream = new ReaderInputStream(from);
         connection.createStatement().sendStreamSQL(inputStream, sql);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void copyToDb(String sql,
+                         Reader from,
+                         Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException {
+        validate(sql, from);
+        ReaderInputStream inputStream = new ReaderInputStream(from);
+        connection.createStatement().sendStreamSQL(inputStream, sql, additionalDBParams);
     }
 
     /**
@@ -62,9 +103,36 @@ final class CopyManagerImpl implements CopyManager, AutoCloseable {
      * {@inheritDoc}
      */
     @Override
+    public void copyToDb(String sql,
+                         Reader from,
+                         Map<ClickHouseQueryParam, String> additionalDBParams,
+                         int bufferSize)
+            throws SQLException {
+        validate(sql, from);
+        ReaderInputStream inputStream = new ReaderInputStream(from);
+        BufferedInputStream bufferedStream = new BufferedInputStream(inputStream, Math.max(32, bufferSize));
+        connection.createStatement().sendStreamSQL(bufferedStream, sql, additionalDBParams);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void copyFromDb(String sql, OutputStream to) throws SQLException {
         validate(sql, to);
         connection.createStatement().sendStreamSQL(sql, to);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void copyFromDb(String sql,
+                           OutputStream to,
+                           Map<ClickHouseQueryParam, String> additionalDBParams)
+            throws SQLException {
+        validate(sql, to);
+        connection.createStatement().sendStreamSQL(sql, to, additionalDBParams);
     }
 
     /**
@@ -75,6 +143,15 @@ final class CopyManagerImpl implements CopyManager, AutoCloseable {
         validate(sql, to);
         WriterOutputStream outputStream = new WriterOutputStream(to);
         connection.createStatement().sendStreamSQL(sql, outputStream);
+    }
+
+    @Override
+    public void copyFromDb(String sql,
+                           Writer to,
+                           Map<ClickHouseQueryParam, String> additionalDBParams) throws SQLException {
+        validate(sql, to);
+        WriterOutputStream outputStream = new WriterOutputStream(to);
+        connection.createStatement().sendStreamSQL(sql, outputStream, additionalDBParams);
     }
 
     private static void validate(String sql, Object stream) throws SQLException {
